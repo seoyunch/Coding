b1a760157fb0d64eadc9ce30f39c458826f9a012
seoyunch <yen731013@naver.com>
2026-02-01 19:44:53 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.18 ms, Memory: 90.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int solution(String s) {
+        StringBuilder str = new StringBuilder(s);
+        StringBuilder numbers = new StringBuilder();
+        String[] words = new String[10];
+        words[0] = "zero";
+        words[1] = "one";
+        words[2] = "two";
+        words[3] = "three";
+        words[4] = "four";
+        words[5] = "five";
+        words[6] = "six";
+        words[7] = "seven";
+        words[8] = "eight";
+        words[9] = "nine";
+
+        while (!str.toString().isEmpty()) {
+            char word = str.charAt(0);
+            String sub;
+
+            if(String.valueOf("0123456789").contains(String.valueOf(word))){
+                numbers.append(word);
+                str.deleteCharAt(0);
+                continue;
+            }
+            if(str.length() > 5){
+                sub = str.substring(0, 5);
+            }else{
+                sub = str.substring(0, str.length());
+            }
+            for(int i=0; i<10; i++){
+                if(sub.contains(words[i])){
+                    numbers.append(i);
+                    str.delete(0, words[i].length());
+                    break;
+                }
+            }
+        }
+        return Integer.parseInt(numbers.toString());
+    }
+} 
