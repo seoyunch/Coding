0bbcb77eb3abe69d06574f6f2f4fb9e4e9c3f61d
seoyunch <yen731013@naver.com>
2026-05-28 23:10:08 +0900

    Update 크기가 작은 부분문자열.java

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -3,6 +3,8 @@ class Solution {
         int answer = 0;
         for(int i=0; i<=t.length()-p.length(); i++){
             String str = t.substring(i,i+p.length());
+            // 처음엔 Integer로 변환했지만 런타임에러 발생
+            // Integer로 변환하는 과정에서 큰 숫자는 깨지는걸 의심하고 Long으로 변환
             if(Long.valueOf(str) <= Long.valueOf(p)){
                 answer++;
             }
